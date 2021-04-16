f08649d17af1b35582a2ae651748177a388ab587
albertsahu92 <54240284+albertsahu92@users.noreply.github.com>
2021-04-16 22:58:45 +0700

    commited full website!

--- js/main.js
+++ js/main.js
@@ -1,80 +1,65 @@
-// document selector
-// const search = document.getElementById('search');
-// const searchList = document.getElementById('search-list');
+const select = document.getElementById('selBreed')
+const disp = document.getElementById('breedDesc')
+// console.log(select)
 
-// search
-//  display test
 
-// async function displayInfo(){
-//     const testText = document.getElementById('info');
-//     const res = await fetch('data/breeds.json')
-//     const test = await res.json();
 
-// console.log(test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// search and filter
+/* const searchBreed = async searchText => {
+  const resp = await fetch('https://api.thecatapi.com/v1/breeds/search');
+  const breeds = await resp.json();
+
+  let match = breeds.filter( breeds => {
+    const regex = new RegExp(`^${searchText}`, 'gi');
+    return breeds.name.match(regex) || breeds.origin.match(regex)
+  });
+  if(searchText.length == 0) {
+    match = ''
+    disp.innerHTML = ''
+  }
+  outputHtml(match)
+
+};
+
+console.log(searchBreed)
+
+const outputHtml = match => {
+  if(match.length > 0) {
+    const html = match.map(match => 
+    `<div class="card card-body mb-1">
+    <h4> ${match.name} (${match.origin}) <span class="text-secondary">
+    ${match.description}</span> </h4>
+    </div>`).join('')
+
+    match.innerHTML = html;
     
-// }
-// displayInfo()
-
-
-// initSelect()
-
-// function initSelect(){
-//     select = $('#selections').select2({
-//         "ajax" : {
-//             "url" : "https://api.thecatapi.com/v1/breeds",
-//             "type" : "GET",
-//             "dataSrc" : "",
-//             "dataType" : "json",
-//         },
-//         "select": [{
-//             "data" : "name"
-//             },
-//             {"data" : "origin"
-
-//             },
-//     ]
-//     });
-    
-// }
-
-
-
-// $('#selections').select2({
-//     ajax: {
-//         url: "http://api.thecatapi.com/v1/breeds",
-//         data: function(breeds) {
-//             var query = {
-//                 search: breeds.origin,
-//                 type: "public"
-//             }
-//             return query;
-//         }
-//     }
-// });
-
-
-// $('#selections').select2({
-//     ajax: {
-//         url: "http://api.thecatapi.com/v1/breeds",
-//         processResults: function(data) {
-//             return{
-//                 results: data.name
-//             }
-//         }
-//     }
-// });
-
-
-// $('#selections').select2({
-//     placeholder: "Select an option here"
-// })
-
-/* problem di select tag html, waktu dicoba console log dengan posisi script main.js di atas, 
-hasilnya 'null' tapi kalau script main.jsnya di bawah, dia dapet element nya*/
-let selectTest = document.getElementById('selections')
-console.log(selectTest);
-
-
-// var selector = document.getElementById("selections");
-// console.log($(selector).select2())
-
+  }
+}
+select.addEventListener('input', () => searchBreed
+(search.value)); */
